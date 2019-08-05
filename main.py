101466b8448a5e0957baab063d9b56c939703aba
Joey Cheung <30446022+JoeyCheung@users.noreply.github.com>
2019-08-05 11:10:36 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -8,17 +8,13 @@ from nltk.corpus import stopwords
 from collections import Counter, OrderedDict
 from sklearn.feature_extraction.text import TfidfVectorizer
 from textblob.sentiments import NaiveBayesAnalyzer
-import time
 os.system('clear')
 
-start = time.time()
-
-inputFilename = 'label.csv'
-outputFilename = 'combinedData_output.csv'
+inputFilename = 'finance.csv'
+outputFilename = 'finance_output.csv'
 
 data = pd.read_csv(inputFilename, header=None)
 data = data.values[1:]
-
 
 def wordCounterGlove():
     """
@@ -47,7 +43,9 @@ stop_words = stopwords.words('english')
 positiveCorpus = []
 negativeCorpus = []
 
+finalResults = []
 for index,sample in enumerate(data):
+    resultArray = []
     temp = word_tokenize(str(sample))
     #temp = [lemmatizer.lemmatize(token.lower()) for token in temp]
     temp = [lemmatizer.lemmatize(token) for token in temp]
@@ -55,23 +53,34 @@ for index,sample in enumerate(data):
                                 len(word) > 2 and word.isalpha()]
     temp = [word for word in temp if word not in stop_words]
     result = predictPolaritySubjectivity(" ".join(temp), [])
-    #print(result)
-    if index == 1:
-        print(result[0])
-    if result[0] + 0.3 > result[1]:
-        positiveCorpus += temp
-    else:
-        negativeCorpus += temp
+    score = result[0]
+    resultArray.append(str(sample[0]))
+    resultArray.append(score)
+    finalResults.append(resultArray)
 
+print(finalResults[0])
+
+
+'''
 print(len(positiveCorpus))
 print(len(negativeCorpus))
+
+
+
+
+
 vectorizer = TfidfVectorizer()
 corpus = []
 corpus.append(" ".join(positiveCorpus))
 corpus.append(" ".join(negativeCorpus))
 tfidf_matrix = vectorizer.fit_transform(corpus)
-df = pd.DataFrame(tfidf_matrix.toarray(), columns = vectorizer.get_feature_names())
-df.to_csv(outputFilename)
 
-end = time.time()
-print(end - start)
+
+'''
+
+
+df = pd.DataFrame(finalResults)
+df.to_csv(outputFilename, encoding='utf-8', index=False, header=None)
+
+
+
